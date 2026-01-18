d58cd4f38a0b1331991aeea3ea0716a25810e6a1
Rahulbbbb <beingbankoti@gmail.com>
2026-01-19 02:27:57 +0530

    v

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
   images: { // <--- Add the 'images' object here
     domains: ['cdn.sanity.io'],
   },
